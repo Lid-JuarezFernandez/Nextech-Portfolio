297ec9845abbea6f9ac40ad85c2cefd254345450
Lidia-0-Juarez-Fernandez <jud3z.l70@gmail.com>
2025-06-16 03:26:03 -0400

    non-working contact

---
+++ script.js
@@ -0,0 +1,15 @@
+let email = document.querySelector(".email");
+let name = document.querySelector(".name");
+let comment = document.querySelector(".comment");
+let send = document.querySelector(".send");
+let received = document.querySelector(".received");
+let newmessage= document.querySelector(".newmessage");
+
+send.addEventListener("click", function() {
+    let email = document.querySelector(".email").value;
+    let name = document.querySelector(".name").value;
+    let send = document.querySelector(".comment").value;
+
+    receive 
+});
+
