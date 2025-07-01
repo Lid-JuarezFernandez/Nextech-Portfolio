f921b4d9e593c9f34579e26da78c304a3bece430
Lidia-0-Juarez-Fernandez <jud3z.l70@gmail.com>
2025-07-01 14:17:54 -0400

    testing push, no visible links, contact, home done, life almost done

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ let spanishbutton = document.querySelector(".spanishbutton");
 
 let intropage = document.querySelector(".introcontainer");
 let aboutpage = document.querySelector(".aboutmecontainer");
-let lifepage = document.querySelector(".lifecontainer")
+let lifepage = document.querySelector(".lifecontainer");
+let lifepagetwo = document.querySelector(".lifecontainertwo");
 let purposepage = document.querySelector(".purposecontainer");
 let workspage = document.querySelector(".workscontainer");
 let infopage = document.querySelector(".infocontainer");
@@ -31,6 +32,7 @@ homebutton.addEventListener("click", function() {
     intropage.style.display = "flex";
     aboutpage.style.display = "flex"
     lifepage.style.display = "none";
+    lifepagetwo.style.display = "none";
     purposepage.style.display = "none";
     workspage.style.display = "none";
     infopage.style.display = "none";
@@ -42,6 +44,7 @@ lifebutton.addEventListener("click", function() {
     intropage.style.display = "none";
     aboutpage.style.display = "none";
     lifepage.style.display = "flex";
+    lifepagetwo.style.display = "flex";
     purposepage.style.display = "none";
     workspage.style.display = "none";
     infopage.style.display = "none";
@@ -52,6 +55,7 @@ purposebutton.addEventListener("click", function() {
     intropage.style.display = "none";
     aboutpage.style.display = "none"
     lifepage.style.display = "none";
+    lifepagetwo.style.display = "none";
     purposepage.style.display = "flex";
     workspage.style.display = "none";
     infopage.style.display = "none";
@@ -62,6 +66,7 @@ worksbutton.addEventListener("click", function() {
     intropage.style.display = "none";
     aboutpage.style.display = "none";
     lifepage.style.display = "none";
+    lifepagetwo.style.display = "none";
     purposepage.style.display = "none";
     workspage.style.display = "flex";
     infopage.style.display = "none";
@@ -73,6 +78,7 @@ contactbutton.addEventListener("click", function() {
     intropage.style.display = "none";
     aboutpage.style.display = "none";
     lifepage.style.display = "none";
+    lifepagetwo.style.display = "none";
     purposepage.style.display = "none";
     workspage.style.display = "none";
     infopage.style.display = "flex";
